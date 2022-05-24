ea7cdac317d3125f453dbffae7e858e14e946072
DESKTOP-C1C2M07\Ivan <iwan.mitowski@gmail.com>
2022-05-24 12:15:12 +0300

    Home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/layout/Layout';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<Layout/>} />
+        <Route exact path="/" element={<Layout/>}>
+          <Route path="/" element={<Home/>}/>
+        </Route>
       </Routes>
     </div>
   );
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,28 @@
-import Navbar from 'react-bootstrap/NavBar';
-import Nav from 'react-bootstrap/Nav';
-import Container from 'react-bootstrap/Container';
-import { Link } from 'react-router-dom';
+import Navbar from "react-bootstrap/NavBar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-    return (
-        <div className="header">
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand><Link className='nav-link' to="/">Rent a car</Link></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                    </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
-    );
+  return (
+    <div className="header">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand>
+            <Link className="nav-link" to="/">
+              Rent a car
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link className="nav-link" to="/">
+                Rent a car
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
